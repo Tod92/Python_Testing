8f54565e4d5008b1dabd916bf477dd830a700b61
Thomas Deruere <t.deruere@gmail.com>
2023-08-02 17:56:09 +0200

    code cleaning and use of flask factory for tests

--- app/app.py
+++ app/app.py
@@ -3,22 +3,15 @@ from flask import Flask,render_template,request,redirect,flash,url_for,Blueprint
 from app.models import Club, Competition
 from app import util
 
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
+    app.config.from_object(config)
     app.secret_key = 'something_special'
     app.register_blueprint(main)
     return app
 
 main = Blueprint("main", __name__)
-
-
-#competitions = Competition.get_competitions_from_json()
-#clubs = Club.get_clubs_from_json()
 
 @main.route('/')
 def index():
@@ -40,25 +33,21 @@ def showSummary():
 
 @main.route('/book/<competition>/<club>')
 def book(competition,club):
-    # foundClub = [c for c in clubs if c['name'] == club][0]
     foundClub = Club.get_club(name=club)
-    # foundCompetition = [c for c in competitions if c['name'] == competition][0]
     foundCompetition = Competition.get_competition(name=competition)
     if foundClub and foundCompetition:
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
+        competitions = Competition.get_competition_list()
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @main.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
-    # competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     competition = Competition.get_competition(name=request.form['competition'])
-    # club = [c for c in clubs if c['name'] == request.form['club']][0]
     club = Club.get_club(name=request.form['club'])
     placesRequired = int(request.form['places'])
-    # competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     sucess = util.book(club, competition, placesRequired)
     if sucess == True:
         flash('Great-booking complete!')
