fdcaa1fb487f8c52f80eb307a40dd79137948503
Thomas Deruere <t.deruere@gmail.com>
2023-08-11 14:39:06 +0200

    test_util.py added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,77 @@
 import pytest
-
+from app import models
 from app.server import create_app
 
 # Data paths for test
 TEST_CLUBS_FILE = "../tests/data/test_clubs.json"
 TEST_COMPETITIONS_FILE = "../tests/data/test_competitions.json"
 TEST_BOOKINGS_FILE = "../tests/data/test_bookings.json"
+
+# Expected results
+EXPECTED_CLUBS = [
+    {
+        "name":"Simply Lift",
+        "email":"john@simplylift.co",
+        "points":"13"
+    },
+    
+    {
+        "name":"Iron Temple",
+        "email": "admin@irontemple.com",
+        "points":"4"
+    },
+    {   "name":"She Lifts",
+        "email": "kate@shelifts.co.uk",
+        "points":"12"
+    }
+    ]
+EXPECTED_COMPETITIONS = [
+    {
+        "name": "Spring Festival",
+        "date": "2020-03-27 10:00:00",
+        "numberOfPlaces": "25"
+    },
+    {
+        "name": "Fall Classic",
+        "date": "2020-10-22 13:30:00",
+        "numberOfPlaces": "13"
+    },
+    {
+        "name": "STRONGMAN Festival",
+        "date": "2023-06-28 10:00:00",
+        "numberOfPlaces": "0"
+    }
+    ]
+EXPECTED_BOOKINGS = [
+    {
+        "name": "Simply Lift", 
+        "booked": 
+        {
+            "Fall Classic": 7, 
+            "STRONGMAN Festival": 3
+        }
+    }, 
+    {
+        "name": "She Lifts", 
+        "booked": 
+        {
+            "Fall Classic": 12, 
+            "Spring Festival": 2
+        }
+    }
+    ]
+
+# Building list of instances with list comprehension
+CLUBS_INSTANCES = [models.Club(name=c['name'],
+                               email=c['email'],
+                               points=c['points']) for c in EXPECTED_CLUBS]
+
+COMPETITIONS_INSTANCES = [models.Competition(name=c['name'],
+                                             date=c['date'],
+                                             numberOfPlaces=c['numberOfPlaces']) for c in EXPECTED_COMPETITIONS]
+
+BOOKINGS_INSTANCES =  [models.Booking(name=b["name"],
+                                      booked=b["booked"]) for b in EXPECTED_BOOKINGS]
 
 
 @pytest.fixture
---
+++ tests/integration_tests/test_util.py
@@ -0,0 +1,83 @@
+from tests.conftest import (
+    TEST_CLUBS_FILE,
+    TEST_COMPETITIONS_FILE,
+    TEST_BOOKINGS_FILE,
+    EXPECTED_CLUBS,
+    EXPECTED_COMPETITIONS,
+    EXPECTED_BOOKINGS,
+    CLUBS_INSTANCES,
+    COMPETITIONS_INSTANCES,
+    BOOKINGS_INSTANCES
+    )
+from app import models, util
+import json
+
+class TestClass:
+    def setup_class(cls):
+        # Club "Iron Temple"
+        cls.club = models.Club(name="Iron Temple",email="mail@mail.com",points="0")
+        # Competition "Spring Festival"
+        cls.competition = models.Competition(name="Spring Festival",date="01012023",numberOfPlaces="0")
+
+    def teardown_class(cls):
+        clubs_json = {models.Club._json_key_name: EXPECTED_CLUBS}
+        file = open(TEST_CLUBS_FILE, 'w')
+        json.dump(clubs_json, file)
+        file.close
+        competitions_json = {models.Competition._json_key_name: EXPECTED_COMPETITIONS}
+        file = open(TEST_COMPETITIONS_FILE, 'w')
+        json.dump(competitions_json, file)
+        file.close
+        bookings_json = {models.Booking._json_key_name: EXPECTED_BOOKINGS}
+        file = open(TEST_BOOKINGS_FILE, 'w')
+        json.dump(bookings_json, file)
+        file.close
+
+    def mocking_json_files(self, mocker):
+        mocker.patch.object(models.Club, '_json_file_path', TEST_CLUBS_FILE)
+        mocker.patch.object(models.Competition, '_json_file_path', TEST_COMPETITIONS_FILE)
+        mocker.patch.object(models.Booking, '_json_file_path', TEST_BOOKINGS_FILE)
+
+    def test_setup(self):
+        assert self.club.name == "Iron Temple"
+        assert self.competition.name == "Spring Festival"
+    
+    def test_should_error_if_not_enough_points(self, mocker):
+        self.mocking_json_files(mocker)
+        self.club.points = 2
+        result = util.book(self.club,self.competition,3)
+        assert type(result) == str
+        assert result == "Club can't afford that many places. Not enough points left."
+
+    def test_should_error_if_not_enough_places(self, mocker):
+        self.mocking_json_files(mocker)
+        self.club.points = 5
+        self.competition.numberOfPlaces = 2
+        result = util.book(self.club,self.competition,3)
+        assert type(result) == str
+        assert result == "Competition has not enough places left to do this"
+
+    def test_should_error_if_more_than_max_authorized_bookings_per_competition(self, mocker):
+        self.mocking_json_files(mocker)
+        self.club.points = 13
+        self.competition.numberOfPlaces = 15
+        mocker.patch.object(util, 'MAX_PLACES_PER_COMP', 12)
+        result = util.book(self.club, self.competition, 13)
+        assert type(result) == str
+        assert result == 'Maximum places per competition for a club is 12'
+        mocker.patch.object(util, 'MAX_PLACES_PER_COMP', 2)
+        result = util.book(self.club, self.competition, 3)        
+        assert type(result) == str
+        assert result == 'Maximum places per competition for a club is 2'
+
+    def test_should_book_and_save(self, mocker):
+        self.mocking_json_files(mocker)
+        self.club.points = 5
+        self.competition.numberOfPlaces = 5
+        self.club.save()
+        self.competition.save()
+        result = util.book(self.club, self.competition, 4)
+        assert result == True
+        booking = models.Booking.get_bookings(self.club.name)
+        assert booking != None
+        assert booking.get_booking(self.competition.name) == 4
